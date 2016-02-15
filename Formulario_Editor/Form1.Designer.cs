39896464fff60d252bfe1cd1cc7291109e89bc9e
JhovanniSaavedra <nirvana_queen19@hotmail.es>
2016-02-15 14:08:09 -0600

    Programa_1

---
+++ Formulario_Editor/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Formulario_Editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.abrirArchivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editarArchivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.crearArchivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarArchivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.LavenderBlush;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirArchivoToolStripMenuItem,
+            this.crearArchivoToolStripMenuItem,
+            this.guardarArchivoToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(605, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // abrirArchivoToolStripMenuItem
+            // 
+            this.abrirArchivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editarArchivoToolStripMenuItem});
+            this.abrirArchivoToolStripMenuItem.Name = "abrirArchivoToolStripMenuItem";
+            this.abrirArchivoToolStripMenuItem.Size = new System.Drawing.Size(89, 20);
+            this.abrirArchivoToolStripMenuItem.Text = "Abrir Archivo";
+            this.abrirArchivoToolStripMenuItem.Click += new System.EventHandler(this.abrirArchivoToolStripMenuItem_Click);
+            // 
+            // editarArchivoToolStripMenuItem
+            // 
+            this.editarArchivoToolStripMenuItem.Name = "editarArchivoToolStripMenuItem";
+            this.editarArchivoToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.editarArchivoToolStripMenuItem.Text = "Editar Archivo";
+            // 
+            // crearArchivoToolStripMenuItem
+            // 
+            this.crearArchivoToolStripMenuItem.Name = "crearArchivoToolStripMenuItem";
+            this.crearArchivoToolStripMenuItem.Size = new System.Drawing.Size(91, 20);
+            this.crearArchivoToolStripMenuItem.Text = "Crear Archivo";
+            this.crearArchivoToolStripMenuItem.Click += new System.EventHandler(this.crearArchivoToolStripMenuItem_Click);
+            // 
+            // guardarArchivoToolStripMenuItem
+            // 
+            this.guardarArchivoToolStripMenuItem.Name = "guardarArchivoToolStripMenuItem";
+            this.guardarArchivoToolStripMenuItem.Size = new System.Drawing.Size(105, 20);
+            this.guardarArchivoToolStripMenuItem.Text = "Guardar Archivo";
+            this.guardarArchivoToolStripMenuItem.Click += new System.EventHandler(this.guardarArchivoToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(0, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(605, 298);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(605, 322);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Editor Jhovanni Saavedra Aguilar";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem crearArchivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirArchivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarArchivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ToolStripMenuItem editarArchivoToolStripMenuItem;
+    }
+}
+
---
+++ Formulario_Editor/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Formulario_Editor
+{
+    public partial class Form1 : Form
+    {
+        String archivo;
+
+        public Form1()
+        {InitializeComponent();}
+
+        private void abrirArchivoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog AbrirArchivo = new OpenFileDialog();
+            AbrirArchivo.Filter = "Texto|*.txt";
+
+            if (AbrirArchivo.ShowDialog() == DialogResult.OK)
+            {
+                archivo = AbrirArchivo.FileName;
+                using (StreamReader leer = new StreamReader(archivo))
+                { richTextBox1.Text = leer.ReadToEnd(); }
+            }
+        }
+
+        private void guardarArchivoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog GuardarArchivo = new SaveFileDialog();
+            GuardarArchivo.Filter = "Texto|*.txt";
+            if (archivo != null)
+            {
+                using (StreamWriter escribir = new StreamWriter(archivo))
+                {escribir.Write(richTextBox1.Text);}
+            }
+
+            else
+            {
+                if (GuardarArchivo.ShowDialog() == DialogResult.OK)
+                {
+                    archivo = GuardarArchivo.FileName;
+                    using (StreamWriter escribir = new StreamWriter(GuardarArchivo.FileName))
+                    {escribir.Write(richTextBox1.Text);
+                    MessageBox.Show("Archivo Guardado");
+                    }
+                }
+            }
+        }
+
+        private void crearArchivoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            archivo = null;
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            if (MessageBox.Show("¿Seguro desea salir?", "Aviso",  MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) == System.Windows.Forms.DialogResult.Yes)
+                {Application.Exit();}
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }// Fin clase publica
+} // Fin llave namespace
